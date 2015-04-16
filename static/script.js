718b37b525b9603a89b0af04505e754c9f59c7d9
Charlie Rode <chrode10@gmail.com>
2015-04-16 16:42:59 -0700

    Convert jQuery time evaluation to a single JS funciton in script.js and update some styling too

--- static/script.js
+++ static/script.js
@@ -1,6 +1,29 @@
+var applyPanelClass = function() {
+    var date, currentDate, startTime, endTime;
+    date = new Date();
+    currentDate = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
+    console.log("currentDate: ", currentDate)
+    startTime = new Date( $(this).data("start-time") + " " + currentDate );
+    endTime = new Date( $(this).data("end-time") + " " + currentDate );
+    console.log("startTime: ", startTime);
+    console.log("endTime: ". endTime);
+    console.log("date > endTime: ", (date > endTime));
+    console.log("date >= startTime: ", (date >= startTime));
+    if (date > endTime) {
+        $(this).addClass('pastEvent');
+        $(this).find('h3').addClass('pastEvent');
+        $(this).switchClass('panel-success', 'panel-default');
+    }
+    else if (date >= startTime) {
+        $(this).switchClass('panel-success', 'panel-warning');
+    }
+}   
+
 $(document).ready(function() {
     $('input[type=radio]').click(function() {
         $(this).closest("form").submit();
     });
+
+    $('div.panel').each(applyPanelClass);
 
 });
